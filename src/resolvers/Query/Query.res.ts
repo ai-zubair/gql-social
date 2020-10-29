30d3f37f215325d436e16651cebe3f0f825faca1
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-10-29 23:03:19 +0530

    Add a post query

--- src/resolvers/Query/Query.res.ts
+++ src/resolvers/Query/Query.res.ts
@@ -1,55 +1,95 @@
-import { IResolverObject } from 'graphql-tools';
-import { ContextWithRequestResponse, EmptyParent } from '../../types/common.type';
+import { IResolverObject, IFieldResolver } from 'graphql-tools';
+import { Context, ContextWithRequestResponse, EmptyArgs, EmptyParent } from '../../types/common.type';
 
 interface QueryArgs{
   keyword: string;
 }
 
-const Query: IResolverObject<EmptyParent, ContextWithRequestResponse, QueryArgs>  = {
-  async posts(parent, args, { prisma }, info){
-    const keyword = args.keyword;
-    const postsWithFilter = await prisma.post.findMany({
-      where: {
-        OR:[
+interface PostQueryArgs{
+  postID: string;
+}
+
+const post: IFieldResolver<EmptyParent, ContextWithRequestResponse, PostQueryArgs> = async(parent, {postID}, {prisma, authenticateUser, request}, info)=>{
+  let requestedPost = await prisma.post.findOne({
+    where:{
+      id: postID
+    }
+  });
+  if(!requestedPost){
+    throw new Error("Post does not exist!")
+  }
+  if(requestedPost.published){
+    return requestedPost;
+  }else{
+    const userID = authenticateUser(request);
+    const [userDraft] = await prisma.post.findMany({
+      where:{
+        AND:[
           {
-            title: {
-              contains: keyword
-            }
+            id: postID
           },
           {
-            body: {
-              contains: keyword
-            }
-          }
-        ]
-      }      
-    })
-    return postsWithFilter;
-  },
-  async users(parent, args, { prisma }, info){
-    const keyword = args.keyword;
-    const usersWithFilter = await prisma.user.findMany({
-      where: {
-        OR:[
-          {
-            name: {
-              contains: keyword
-            }
-          },
-          {
-            email: {
-              contains: keyword
-            }
+            authorId: userID
           }
         ]
       }
-    })
-    return usersWithFilter;
-  },
-  async comments(parent, args, { prisma }, info){
-    const comments = await prisma.comment.findMany();
-    return comments;
+    });
+    return userDraft;
   }
 }
 
-export { Query };
+const posts: IFieldResolver<EmptyParent, ContextWithRequestResponse, QueryArgs> = async(parent, args, { prisma }, info)=>{
+  const keyword = args.keyword;
+  const postsWithFilter = await prisma.post.findMany({
+    where: {
+      OR:[
+        {
+          title: {
+            contains: keyword
+          }
+        },
+        {
+          body: {
+            contains: keyword
+          }
+        }
+      ]
+    }      
+  })
+  return postsWithFilter;
+}
+
+const users: IFieldResolver<EmptyParent, ContextWithRequestResponse, QueryArgs> = async(parent, args, { prisma }, info)=>{
+  const keyword = args.keyword;
+  const usersWithFilter = await prisma.user.findMany({
+    where: {
+      OR:[
+        {
+          name: {
+            contains: keyword
+          }
+        },
+        {
+          email: {
+            contains: keyword
+          }
+        }
+      ]
+    }
+  })
+  return usersWithFilter;
+}
+
+const comments: IFieldResolver<EmptyParent, ContextWithRequestResponse, EmptyArgs> = async(parent, args, { prisma }, info)=>{
+  const comments = await prisma.comment.findMany();
+  return comments;
+}
+
+const Query = { 
+  posts,
+  post,
+  users,
+  comments
+ };
+
+ export { Query };
