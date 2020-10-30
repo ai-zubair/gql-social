887cfabd62109031fc58da9c986c7e76a22b1d7e
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-10-30 15:19:52 +0530

    Lock down subscriptions

--- src/resolvers/Subscription/Subscription.res.ts
+++ src/resolvers/Subscription/Subscription.res.ts
@@ -1,34 +1,38 @@
 import { IResolverObject, IResolverOptions } from 'graphql-tools';
+import { authenticateUser } from '../../context';
 import { EmptyParent, ContextWithRequestResponse} from '../../types/common.type';
 import { CommentSubscriptionArgs, PostSubscriptionArgs } from '../../types/subscription.type';
 
 const comment: IResolverOptions<EmptyParent, ContextWithRequestResponse, CommentSubscriptionArgs> = {
-  async subscribe(parent, { postID }, { prisma, pubSub }, info){
-    const postToSubscribeTo = await prisma.post.findOne({
-      where:{
-        id: postID
+  async subscribe(parent, { postID }, { prisma, pubSub, connection }, info){
+    if(authenticateUser(connection.context)){
+      const postToSubscribeTo = await prisma.post.findOne({
+        where:{
+          id: postID
+        }
+      })
+      if(postToSubscribeTo){
+        return pubSub.asyncIterator(postID);
+      }else{
+        throw new Error("Post does not exist!")
       }
-    })
-    if(postToSubscribeTo){
-      console.log("Subscribed to the post");
-      return pubSub.asyncIterator(postID);
-    }else{
-      throw new Error("Post does not exist!")
     }
   }
 }
 
 const post: IResolverOptions<EmptyParent, ContextWithRequestResponse, PostSubscriptionArgs> = {
-  async subscribe(parent, { userID }, { prisma, pubSub }, info){
-    const userToSubscribeTo = await prisma.user.findOne({
-      where:{
-        id: userID
+  async subscribe(parent, { userID }, { prisma, pubSub, connection }, info){
+    if(authenticateUser(connection.context)){
+      const userToSubscribeTo = await prisma.user.findOne({
+        where:{
+          id: userID
+        }
+      })
+      if(userToSubscribeTo){
+        return pubSub.asyncIterator(userID);
+      }else{
+        throw new Error("User does not exist!")
       }
-    })
-    if(userToSubscribeTo){
-      return pubSub.asyncIterator(userID);
-    }else{
-      throw new Error("User does not exist!")
     }
   }
 } 
